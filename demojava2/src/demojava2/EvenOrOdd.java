ab820c77d4730dc49633c0bc6f33179a2ff6f71d
vedivi <vedivi@inlen8520016688.corp.capgemini.com>
2022-04-13 13:45:22 +0530

    added evenorodd class

---
+++ demojava2/src/demojava2/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package demojava2;
+
+public class EvenOrOdd {
+public static void main(String[] args) {
+	int x = 20;
+	if(x%2==0)
+	{
+		System.out.println("even number");
+	}
+	else
+	{
+		System.out.println("odd number");
+	}
+}
+}
